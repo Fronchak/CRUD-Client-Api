1c27a46aee234fc3c450459cd5aaea9274ef64b9
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-12-02 19:30:05 -0300

    Implement method to delete a client

--- src/main/java/com/fronchak/company/domain/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/com/fronchak/company/domain/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.fronchak.company.domain.exceptions.DatabaseException;
 import com.fronchak.company.domain.exceptions.ExceptionResponse;
 import com.fronchak.company.domain.exceptions.ResourceNotFoundException;
 
@@ -20,13 +21,26 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(
 			ResourceNotFoundException e, WebRequest request) {
-		ExceptionResponse response = new ExceptionResponse();
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		ExceptionResponse response = buildResponse(e, request, "Resource not found", status);
+		return ResponseEntity.status(status).body(response);
+	}
+	
+	private ExceptionResponse buildResponse(Exception e, WebRequest request, String error, HttpStatus status) {
+		ExceptionResponse response = new ExceptionResponse();
 		response.setTimestamp(Instant.now());
-		response.setError("Resource not found");
+		response.setError(error);
 		response.setMessage(e.getMessage());
 		response.setStatus(status.value());
 		response.setPath(request.getDescription(false));
+		return response;
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<ExceptionResponse> handleDatabaseException(
+			DatabaseException e, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ExceptionResponse response = buildResponse(e, request, "Integrity violation", status);
 		return ResponseEntity.status(status).body(response);
 	}
 }
--- src/main/java/com/fronchak/company/domain/services/ClientService.java
+++ src/main/java/com/fronchak/company/domain/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.fronchak.company.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -8,9 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fronchak.company.api.dtos.ClientDTO;
 import com.fronchak.company.domain.entities.Client;
+import com.fronchak.company.domain.exceptions.DatabaseException;
 import com.fronchak.company.domain.exceptions.ResourceNotFoundException;
 import com.fronchak.company.domain.mappers.ClientMapper;
 import com.fronchak.company.domain.repositories.ClientRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -40,5 +45,30 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest request) {
 		Page<Client> entities = repository.findAll(request);
 		return mapper.convertEntityPageToDTOPage(entities);
+	}
+	
+	@Transactional
+	public ClientDTO update(ClientDTO dto, Long id) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			mapper.copyDTOToEntity(dto, entity);
+			entity = repository.save(entity);
+			return mapper.convertEntityToDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Client not found by ID: " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Client not found by ID: " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Database integrity violation");
+		}
 	}
 }
